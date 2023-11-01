ed268a1e2389dfff74664198e1c45a3a04f23ceb
angularpackage <sciborrudnicki@wvvw.dev>
2023-11-01 17:09:20 +0000

    chore(gulpfile.js): update.

--- gulpfile.js
+++ gulpfile.js
@@ -8,77 +8,160 @@ const run = require('gulp-run');
 const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
 
+/**
+ * Build main spectre CSS files.
+ * @returns
+ */
 function build() {
   return gulp
-    .src('./src/*.scss')
-    .pipe(sass({outputStyle: 'expanded'})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
+    .src([
+      './src/spectre.core.scss',
+      './src/spectre.css.scss',
+      // './src/spectre.scss',
+      // './src/spectre.exp.scss',
+      // './src/spectre.icons.scss',
+    ])
+    .pipe(sass({outputStyle: 'expanded', includePaths: ['node_modules']}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
     .pipe(gulp.dest('./dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(cleancss({inline: ['none']}))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist'));
 }
 
-function build_colors(cb) {
-  const modules = [
-    'buttons',
-    'hero',
-    'labels',
-    'pagination',
-    'toasts'
-  ];
-
-  modules.forEach(function(module) {
-    gulp
-    .src([
-      './src/' + module + '/colors/*.scss',
-      './src/' + module + '/colors/**/*.scss'
-    ])
-    .pipe(sass({outputStyle: 'expanded'})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./dist/' + module))
-    .pipe(gulp.dest('./docs/dist/' + module))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist/' + module))
-    .pipe(gulp.dest('./docs/dist/' + module));
+/**
+ * Build Spectre CSS modules files.
+ * @param {*} cb
+ */
+function build_module(cb) {
+  var modules = [];
+  var i = 0;
+  process.argv.find(element => {
+    if (element.includes('--module')) {
+      const selected_modules = element.split('--module=').slice(1)[0].split(',');
+      if (selected_modules.length > 0) {
+        modules = selected_modules;
+      }
+    }
+    i++;
   });
+  if (modules.length == 0) {
+    modules = [
+      'css-variables',
+      'accordions',
+      'animations',
+      'avatars',
+      'badges',
+      'bars',
+      'bg',
+      'borders',
+      'breadcrumbs',
+      'buttons',
+      'calendars',
+      'cards',
+      'carousels',
+      'chips',
+      'codes',
+      'dots',
+      'dropdowns',
+      'empty',
+      'footer',
+      'forms',
+      'hero',
+      'icons',
+      'labels',
+      'layout',
+      'menus',
+      'meters',
+      'modals',
+      'navbar',
+      'navs',
+      'off-canvas',
+      'overlays',
+      'paginations',
+      'panels',
+      'parallax',
+      'popovers',
+      'progress',
+      'sidebars',
+      'sliders',
+      'steps',
+      'tables',
+      'tabs',
+      'test',
+      'text',
+      'tiles',
+      'timelines',
+      'toasts',
+      'tooltips',
+      'utilities'
+    ];
+  }
+
+  if (modules.length > 0) {
+    modules.forEach(function(module) {
+      gulp
+      .src([
+        './src/' + module + '/*.scss',
+        './src/' + module + '/**/*.scss',
+        './src/' + module + '/**/**/*.scss',
+      ])
+      .pipe(sass({outputStyle: 'expanded', includePaths: ['node_modules']}).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./dist/' + module))
+      .pipe(cleancss({inline: ['none']}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./dist/' + module));
+    });
+  }
 
   cb();
 }
 
+/**
+ * Clean production folder.
+ * @param {*} cb
+ */
 function dist_clean(cb) {
   run('npm run remove:prod:dist').exec();
   cb();
 }
 
+/**
+ * Clean documentation folder CSS and HTML files.
+ * @param {*} cb
+ */
 function docs_clean(cb) {
-  docs_clean_dist(cb);
+  docs_clean_css(cb);
   docs_clean_html(cb);
   cb();
 }
 
-function docs_clean_dist(cb) {
+/**
+ * Clean production folder in the documentation.
+ * @param {*} cb
+ */
+function docs_clean_css(cb) {
   run('npm run remove:docs:dist').exec();
   cb();
 }
 
+/**
+ * Clean HTML documentation files.
+ * @param {*} cb
+ */
 function docs_clean_html(cb) {
   run('npm run remove:docs:html').exec();
   cb();
 }
 
+/**
+ * Build documentation CSS files.
+ * @param {*} cb
+ */
 function build_docs_css(cb) {
   gulp
-    .src(['./src/*.scss', './docs/src/scss/*.scss'])
+    .src(['./docs/src/scss/*.scss'])
     .pipe(sass.sync({outputStyle: 'expanded', precision: 10})
       .on('error', sass.logError)
     )
@@ -93,6 +176,10 @@ function build_docs_css(cb) {
   cb();
 }
 
+/**
+ * Build HTML documentation files.
+ * @returns
+ */
 function build_docs_html() {
   return gulp
     .src('docs/src/**/!(_)*.pug')
@@ -102,15 +189,20 @@ function build_docs_html() {
     .pipe(gulp.dest('./docs/'));
 }
 
+/**
+ * Watch the build.
+ */
 function watch() {
   gulp.watch('./**/*.scss', parallel(build, build_docs_css));
   gulp.watch('./**/*.pug', build_docs_html);
 }
 
 exports.watch = watch;
-exports.build = series(dist_clean, parallel(build, build_colors));
-exports.build_colors = build_colors;
-exports.build_docs = series(docs_clean, parallel(build_docs_css, build_colors, build_docs_html));
-exports.build_docs_css = series(docs_clean_dist, parallel(build_docs_css, build_colors));
+exports.build = series(dist_clean, parallel(build, build_module));
+exports.build_module = build_module;
+exports.build_dist = series(build);
+exports.build_docs = series(docs_clean, parallel(build_docs_css, build_docs_html));
+exports.build_docs_css = series(docs_clean_css, parallel(build_docs_css));
 exports.build_docs_html = series(docs_clean_html, parallel(build_docs_html));
 exports.default = build;
+exports.clean_docs = docs_clean;
